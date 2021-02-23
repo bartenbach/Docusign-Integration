2d7c3568f22751bcbed5f0f72a1068ecaff887a7
Larimitus <hmendezvallejo@gmail.com>
2021-02-23 02:45:18 -0600

    feat: api/all endpoint implementation into the ReactJS client chore: update .gitignore & configured build.gradle docs: update README

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -1,22 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 
+function base64ToBlob(base64) {
+    const binaryString = window.atob(base64);
+    const len = binaryString.length;
+    const bytes = new Uint8Array(len);
+    for (let i = 0; i < len; ++i) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+
+    return new Blob([bytes], { type: 'application/pdf' });
+}
+
+function getApi() {
+  console.log('The \'Get Api\' button was clicked!');
+  fetch('/api/all')
+      .then(response => response.json())
+      .then(data => {
+          console.log(data);
+          document.getElementById('builderName').textContent = data[0].builderName;
+          document.getElementById('pdfDoc').src = URL.createObjectURL(base64ToBlob(data[0].document));
+      })
+      .catch(err => console.error(err));
+  console.log('Everything was done.');
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="col-right">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Edit <code>src/App.js</code> and save to reload.</p>
+          <button onClick={getApi}>Fetch API</button>
+        </div>
+        <div className="col-left">
+          <p><code>PDF Author:</code> <code id="builderName"></code></p>
+          <iframe id="pdfDoc" src=""></iframe>
+        </div>
       </header>
     </div>
   );
