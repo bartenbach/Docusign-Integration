b6976735d06c3eb815110b268aabfb5ccdb4e7fc
Blake Bartenbach <blakebartenbach@gmail.com>
2021-03-01 19:04:30 -0600

    test: Adding other changed files (whoops)

--- src/main/java/edu/unomaha/docusign/interfaces/DocumentService.java
+++ src/main/java/edu/unomaha/docusign/interfaces/DocumentService.java
@@ -2,12 +2,12 @@ package edu.unomaha.docusign.interfaces;
 
 import edu.unomaha.docusign.entities.Document;
 
-import javax.print.Doc;
 import java.util.List;
 
 public interface DocumentService {
 
-    public List<Document> getDocumentsByBuilder(String builderName);
-    public List<Document> getAllDocuments();
-    public void addNewDocument(Document document);
+    List<Document> getDocumentsByBuilder(String builderName);
+    List<Document> getAllDocuments();
+    void addNewDocument(Document document);
+
 }
