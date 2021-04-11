446197ceb5f72b3b6f5025212d834d41db042291
rrmarquis <rrmarquis@unomaha.com>
2021-04-10 21:07:01 -0500

    formatting

--- src/main/webapp/src/components/embeddedSending.tsx
+++ src/main/webapp/src/components/embeddedSending.tsx
@@ -2,44 +2,73 @@ import BtButton from "./BtButton";
 import "../App.css";
 
 export default function EmbeddedSending() {
-    return (
-        
-        <div className="container"> 
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossOrigin="anonymous">
-        </link>
-        <form className="eg" action="" method="post" data-busy="form">
+  return (
+    <div className="container">
+      <link
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossOrigin="anonymous"
+      ></link>
+      <form className="eg" action="" method="post" data-busy="form">
         <div className="form-group">
-            <label htmlFor="startingView">Starting View</label>
-            <select id="startingView" name="startingView" className="form-control">
-            <option value="tagging" selected>Tagging view</option>
+          <label htmlFor="startingView">Starting View</label>
+          <select
+            id="startingView"
+            name="startingView"
+            className="form-control"
+          >
+            <option value="tagging" selected>
+              Tagging view
+            </option>
             <option value="recipient">Recipient &amp; Documents view</option>
-            </select>
+          </select>
         </div>
         <div className="form-group">
-            <label htmlFor="signerEmail">Signer Email</label>
-            <input type="email" id="signerEmail" name="signerEmail" 
-            className="form-control" placeholder="email@domain.com">
-            </input>
+          <label htmlFor="signerEmail">Signer Email</label>
+          <input
+            type="email"
+            id="signerEmail"
+            name="signerEmail"
+            className="form-control"
+            placeholder="email@domain.com"
+          ></input>
         </div>
         <div className="form-group">
-            <label htmlFor="signerName">Signer Name</label>
-            <input type="text" id="signerName" placeholder="FirstName LastName" 
-            className="form-control" name="signerName"></input>
+          <label htmlFor="signerName">Signer Name</label>
+          <input
+            type="text"
+            id="signerName"
+            placeholder="FirstName LastName"
+            className="form-control"
+            name="signerName"
+          ></input>
         </div>
         <div className="form-group">
-            <label htmlFor="ccEmail">CC Email</label>
-            <input type="email" id="ccEmail" className="form-control"
-            name="ccEmail" placeholder="email@domain.com" required></input>
+          <label htmlFor="ccEmail">CC Email</label>
+          <input
+            type="email"
+            id="ccEmail"
+            className="form-control"
+            name="ccEmail"
+            placeholder="email@domain.com"
+            required
+          ></input>
         </div>
         <div className="form-group">
-            <label htmlFor="ccName">CC Name</label>
-            <input type="text" id="ccName" placeholder="FirstName LastName"
-            className="form-control" name="ccName" required></input>
+          <label htmlFor="ccName">CC Name</label>
+          <input
+            type="text"
+            id="ccName"
+            placeholder="FirstName LastName"
+            className="form-control"
+            name="ccName"
+            required
+          ></input>
         </div>
         <input type="hidden" name="_csrf" value="<%- csrfToken %>"></input>
-            <BtButton text="Submit" link="/" />
-        </form>
-
-        </div>
-    );
+        <BtButton text="Submit" link="/" />
+      </form>
+    </div>
+  );
 }
--- src/main/webapp/src/config/index.js
+++ src/main/webapp/src/config/index.js
@@ -1,11 +1,12 @@
-const settings = require('./appsettings.json');
-exports.docNames = require('./documentNames.json');
+const settings = require("./appsettings.json");
+exports.docNames = require("./documentNames.json");
 
 const dsOauthServer = settings.production
-    ? 'https://account.docusign.com'
-    : 'https://account-d.docusign.com';
+  ? "https://account.docusign.com"
+  : "https://account-d.docusign.com";
 
-settings.dsClientSecret = process.env.DS_CLIENT_SECRET || settings.dsClientSecret;
+settings.dsClientSecret =
+  process.env.DS_CLIENT_SECRET || settings.dsClientSecret;
 settings.signerEmail = process.env.DS_SIGNER_EMAIL || settings.signerEmail;
 settings.signerName = process.env.DS_SIGNER_NAME || settings.signerName;
 settings.dsClientId = process.env.DS_CLIENT_ID || settings.dsClientId;
@@ -13,5 +14,5 @@ settings.appUrl = process.env.DS_APP_URL || settings.appUrl;
 
 exports.config = {
   dsOauthServer,
-  ...settings
+  ...settings,
 };
